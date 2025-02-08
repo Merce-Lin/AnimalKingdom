06313941b8aae90b6540e2df84adae577ce8ab84
Merce-Lin <m3rcillicious@gmail.com>
2025-02-08 17:52:25 +0800

    Create scripts.js

---
+++ Land/scripts.js
@@ -0,0 +1,476 @@
+function SiameseCatInfo(){
+    document.getElementById('siamese_cat-head').style.top = '0';
+    document.getElementById('siamese_cat-info').style.bottom = '0';
+} 
+function SiameseCatClose(){
+    document.getElementById('siamese_cat-head').style.top = '-100%';
+    document.getElementById('siamese_cat-info').style.bottom = '-100%'; 
+}
+
+function BritishShorthairInfo(){
+    document.getElementById('british_shorthair-head').style.top = '0';
+    document.getElementById('british_shorthair-info').style.bottom = '0';
+}
+function BritishShorthairClose(){
+    document.getElementById('british_shorthair-head').style.top = '-100%';
+    document.getElementById('british_shorthair-info').style.bottom = '-100%';
+}
+
+function MaineCoonInfo(){
+    document.getElementById('maine_coon-head').style.top = '0';
+    document.getElementById('maine_coon-info').style.bottom = '0';
+}
+function MaineCoonClose(){
+    document.getElementById('maine_coon-head').style.top = '-100%';
+    document.getElementById('maine_coon-info').style.bottom = '-100%'; 
+}
+
+function PersianCatInfo(){
+    document.getElementById('persian_cat-head').style.top = '0';
+    document.getElementById('persian_cat-info').style.bottom = '0';
+}
+function PersianCatClose(){
+    document.getElementById('persian_cat-head').style.top = '-100%';
+    document.getElementById('persian_cat-info').style.bottom = '-100%'; 
+}
+
+function RagdollInfo(){
+    document.getElementById('ragdoll-head').style.top = '0';
+    document.getElementById('ragdoll-info').style.bottom = '0';
+}
+function RagdollClose(){
+    document.getElementById('ragdoll-head').style.top = '-100%';
+    document.getElementById('ragdoll-info').style.bottom = '-100%'; 
+}
+
+function SphynxCatInfo() {
+    document.getElementById('sphynx_cat-head').style.top = '0';
+    document.getElementById('sphynx_cat-info').style.bottom = '0';
+}
+function SphynxCatClose() {
+    document.getElementById('sphynx_cat-head').style.top = '-100%';
+    document.getElementById('sphynx_cat-info').style.bottom = '-100%'; 
+}
+
+function AbyssinianCatInfo() {
+    document.getElementById('abyssinian-cat-head').style.top = '0';
+    document.getElementById('abyssinian-cat-info').style.bottom = '0';
+}
+function AbyssinianCatClose() {
+    document.getElementById('abyssinian-cat-head').style.top = '-100%';
+    document.getElementById('abyssinian-cat-info').style.bottom = '-100%'; 
+}
+
+function AmericanShorthairInfo() {
+    document.getElementById('american-shorthair-head').style.top = '0';
+    document.getElementById('american-shorthair-info').style.bottom = '0';
+}
+function AmericanShorthairClose() {
+    document.getElementById('american-shorthair-head').style.top = '-100%';
+    document.getElementById('american-shorthair-info').style.bottom = '-100%'; 
+}
+
+function BurmeseCatInfo() {
+    document.getElementById('burmese-cat-head').style.top = '0';
+    document.getElementById('burmese-cat-info').style.bottom = '0';
+}
+function BurmeseCatClose() {
+    document.getElementById('burmese-cat-head').style.top = '-100%';
+    document.getElementById('burmese-cat-info').style.bottom = '-100%'; 
+}
+
+function ExoticShorthairInfo() {
+    document.getElementById('exotic-shorthair-head').style.top = '0';
+    document.getElementById('exotic-shorthair-info').style.bottom = '0';
+}
+function ExoticShorthairClose() {
+    document.getElementById('exotic-shorthair-head').style.top = '-100%';
+    document.getElementById('exotic-shorthair-info').style.bottom = '-100%'; 
+}
+
+function BirmanInfo() {
+    document.getElementById('birman-head').style.top = '0';
+    document.getElementById('birman-info').style.bottom = '0';
+}
+function BirmanClose() {
+    document.getElementById('birman-head').style.top = '-100%';
+    document.getElementById('birman-info').style.bottom = '-100%'; 
+}
+
+function ScottishFoldInfo() {
+    document.getElementById('scottish-fold-head').style.top = '0';
+    document.getElementById('scottish-fold-info').style.bottom = '0';
+}
+function ScottishFoldClose() {
+    document.getElementById('scottish-fold-head').style.top = '-100%';
+    document.getElementById('scottish-fold-info').style.bottom = '-100%'; 
+}
+
+function BombayCatInfo() {
+    document.getElementById('bombay-cat-head').style.top = '0';
+    document.getElementById('bombay-cat-info').style.bottom = '0';
+}
+function BombayCatClose() {
+    document.getElementById('bombay-cat-head').style.top = '-100%';
+    document.getElementById('bombay-cat-info').style.bottom = '-100%'; 
+}
+
+function SiberianCatInfo() {
+    document.getElementById('siberian-cat-head').style.top = '0';
+    document.getElementById('siberian-cat-info').style.bottom = '0';
+}
+function SiberianCatClose() {
+    document.getElementById('siberian-cat-head').style.top = '-100%';
+    document.getElementById('siberian-cat-info').style.bottom = '-100%'; 
+}
+
+function RussianBlueInfo() {
+    document.getElementById('russian-blue-head').style.top = '0';
+    document.getElementById('russian-blue-info').style.bottom = '0';
+}
+function RussianBlueClose() {
+    document.getElementById('russian-blue-head').style.top = '-100%';
+    document.getElementById('russian-blue-info').style.bottom = '-100%'; 
+}
+
+function NorwegianForestCatInfo() {
+    document.getElementById('norwegian-forest-cat-head').style.top = '0';
+    document.getElementById('norwegian-forest-cat-info').style.bottom = '0';
+}
+function NorwegianForestCatClose() {
+    document.getElementById('norwegian-forest-cat-head').style.top = '-100%';
+    document.getElementById('norwegian-forest-cat-info').style.bottom = '-100%'; 
+}
+
+function DevonRexInfo() {
+    document.getElementById('devon-rex-head').style.top = '0';
+    document.getElementById('devon-rex-info').style.bottom = '0';
+}
+function DevonRexClose() {
+    document.getElementById('devon-rex-head').style.top = '-100%';
+    document.getElementById('devon-rex-info').style.bottom = '-100%'; 
+}
+
+function MunchkinCatInfo() {
+    document.getElementById('munchkin-cat-head').style.top = '0';
+    document.getElementById('munchkin-cat-info').style.bottom = '0';
+}
+function MunchkinCatClose() {
+    document.getElementById('munchkin-cat-head').style.top = '-100%';
+    document.getElementById('munchkin-cat-info').style.bottom = '-100%'; 
+}
+
+function AmericanCurlInfo() {
+    document.getElementById('american-curl-head').style.top = '0';
+    document.getElementById('american-curl-info').style.bottom = '0';
+}
+function AmericanCurlClose() {
+    document.getElementById('american-curl-head').style.top = '-100%';
+    document.getElementById('american-curl-info').style.bottom = '-100%'; 
+}
+
+function AmericanBobtailInfo() {
+    document.getElementById('american-bobtail-head').style.top = '0';
+    document.getElementById('american-bobtail-info').style.bottom = '0';
+}
+function AmericanBobtailClose() {
+    document.getElementById('american-bobtail-head').style.top = '-100%';
+    document.getElementById('american-bobtail-info').style.bottom = '-100%'; 
+}
+
+function BalineseCatInfo() {
+    document.getElementById('balinese-cat-head').style.top = '0';
+    document.getElementById('balinese-cat-info').style.bottom = '0';
+}
+function BalineseCatClose() {
+    document.getElementById('balinese-cat-head').style.top = '-100%';
+    document.getElementById('balinese-cat-info').style.bottom = '-100%'; 
+}
+
+function OrientalShorthairInfo() {
+    document.getElementById('oriental-shorthair-head').style.top = '0';
+    document.getElementById('oriental-shorthair-info').style.bottom = '0';
+}
+function OrientalShorthairClose() {
+    document.getElementById('oriental-shorthair-head').style.top = '-100%';
+    document.getElementById('oriental-shorthair-info').style.bottom = '-100%'; 
+}
+
+function ChartreuxInfo() {
+    document.getElementById('chartreux-head').style.top = '0';
+    document.getElementById('chartreux-info').style.bottom = '0';
+}
+function ChartreuxClose() {
+    document.getElementById('chartreux-head').style.top = '-100%';
+    document.getElementById('chartreux-info').style.bottom = '-100%'; 
+}
+
+function TurkishAngoraInfo(){
+    document.getElementById('turkish_angora-head').style.top = '0';
+    document.getElementById('turkish_angora-info').style.bottom = '0';
+}
+function TurkishAngoraClose(){
+    document.getElementById('turkish_angora-head').style.top = '-100%';
+    document.getElementById('turkish_angora-info').style.bottom = '-100%'; 
+}
+
+function JapaneseBobtailInfo(){
+    document.getElementById('japanese_bobtail-head').style.top = '0';
+    document.getElementById('japanese_bobtail-info').style.bottom = '0';
+}
+function JapaneseBobtailClose(){
+    document.getElementById('japanese_bobtail-head').style.top = '-100%';
+    document.getElementById('japanese_bobtail-info').style.bottom = '-100%'; 
+}
+
+function ManxCatInfo(){
+    document.getElementById('manx_cat-head').style.top = '0';
+    document.getElementById('manx_cat-info').style.bottom = '0';
+}
+function ManxCatClose(){
+    document.getElementById('manx_cat-head').style.top = '-100%';
+    document.getElementById('manx_cat-info').style.bottom = '-100%'; 
+}
+
+function AmericanWirehairInfo(){
+    document.getElementById('american_wirehair-head').style.top = '0';
+    document.getElementById('american_wirehair-info').style.bottom = '0';
+}
+function AmericanWirehairClose(){
+    document.getElementById('american_wirehair-head').style.top = '-100%';
+    document.getElementById('american_wirehair-info').style.bottom = '-100%'; 
+}
+
+function EgyptianMauInfo(){
+    document.getElementById('egyptian_mau-head').style.top = '0';
+    document.getElementById('egyptian_mau-info').style.bottom = '0';
+}
+function EgyptianMauClose(){
+    document.getElementById('egyptian_mau-head').style.top = '-100%';
+    document.getElementById('egyptian_mau-info').style.bottom = '-100%'; 
+}
+
+function RagamuffinInfo(){
+    document.getElementById('ragamuffin-head').style.top = '0';
+    document.getElementById('ragamuffin-info').style.bottom = '0';
+}
+function RagamuffinClose(){
+    document.getElementById('ragamuffin-head').style.top = '-100%';
+    document.getElementById('ragamuffin-info').style.bottom = '-100%'; 
+}
+
+function CornishRexInfo(){
+    document.getElementById('cornish_rex-head').style.top = '0';
+    document.getElementById('cornish_rex-info').style.bottom = '0';
+}
+function CornishRexClose(){
+    document.getElementById('cornish_rex-head').style.top = '-100%';
+    document.getElementById('cornish_rex-info').style.bottom = '-100%'; 
+}
+
+function SomaliCatInfo(){
+    document.getElementById('somali_cat-head').style.top = '0';
+    document.getElementById('somali_cat-info').style.bottom = '0';
+}
+function SomaliCatClose(){
+    document.getElementById('somali_cat-head').style.top = '-100%';
+    document.getElementById('somali_cat-info').style.bottom = '-100%'; 
+}
+
+function HimalayanCatInfo(){
+    document.getElementById('himalayan_cat-head').style.top = '0';
+    document.getElementById('himalayan_cat-info').style.bottom = '0';
+}
+function HimalayanCatClose(){
+    document.getElementById('himalayan_cat-head').style.top = '-100%';
+    document.getElementById('himalayan_cat-info').style.bottom = '-100%'; 
+}
+
+function SelkirkRexInfo(){
+    document.getElementById('selkirk_rex-head').style.top = '0';
+    document.getElementById('selkirk_rex-info').style.bottom = '0';
+}
+function SelkirkRexClose(){
+    document.getElementById('selkirk_rex-head').style.top = '-100%';
+    document.getElementById('selkirk_rex-info').style.bottom = '-100%'; 
+}
+
+function KoratInfo(){
+    document.getElementById('korat-head').style.top = '0';
+    document.getElementById('korat-info').style.bottom = '0';
+}
+function KoratClose(){
+    document.getElementById('korat-head').style.top = '-100%';
+    document.getElementById('korat-info').style.bottom = '-100%'; 
+}
+
+function OcicatInfo(){
+    document.getElementById('ocicat-head').style.top = '0';
+    document.getElementById('ocicat-info').style.bottom = '0';
+}
+function OcicatClose(){
+    document.getElementById('ocicat-head').style.top = '-100%';
+    document.getElementById('ocicat-info').style.bottom = '-100%'; 
+}
+
+function SingapuraCatInfo(){
+    document.getElementById('singapura_cat-head').style.top = '0';
+    document.getElementById('singapura_cat-info').style.bottom = '0';
+}
+function SingapuraCatClose(){
+    document.getElementById('singapura_cat-head').style.top = '-100%';
+    document.getElementById('singapura_cat-info').style.bottom = '-100%'; 
+}
+
+function TonkineseCatInfo(){
+    document.getElementById('tonkinese_cat-head').style.top = '0';
+    document.getElementById('tonkinese_cat-info').style.bottom = '0';
+}
+function TonkineseCatClose(){
+    document.getElementById('tonkinese_cat-head').style.top = '-100%';
+    document.getElementById('tonkinese_cat-info').style.bottom = '-100%'; 
+}
+
+function TurkishVanInfo(){
+    document.getElementById('turkish_van-head').style.top = '0';
+    document.getElementById('turkish_van-info').style.bottom = '0';
+}
+function TurkishVanClose(){
+    document.getElementById('turkish_van-head').style.top = '-100%';
+    document.getElementById('turkish_van-info').style.bottom = '-100%'; 
+}
+
+function BritishLonghairInfo(){
+    document.getElementById('british_longhair-head').style.top = '0';
+    document.getElementById('british_longhair-info').style.bottom = '0';
+}
+function BritishLonghairClose(){
+    document.getElementById('british_longhair-head').style.top = '-100%';
+    document.getElementById('british_longhair-info').style.bottom = '-100%'; 
+}
+
+function HavanaBrownInfo(){
+    document.getElementById('havana_brown-head').style.top = '0';
+    document.getElementById('havana_brown-info').style.bottom = '0';
+}
+function HavanaBrownClose(){
+    document.getElementById('havana_brown-head').style.top = '-100%';
+    document.getElementById('havana_brown-info').style.bottom = '-100%'; 
+}
+
+function ChausieInfo(){
+    document.getElementById('chausie-head').style.top = '0';
+    document.getElementById('chausie-info').style.bottom = '0';
+}
+function ChausieClose(){
+    document.getElementById('chausie-head').style.top = '-100%';
+    document.getElementById('chausie-info').style.bottom = '-100%'; 
+}
+
+function LaPermInfo(){
+    document.getElementById('laperm-head').style.top = '0';
+    document.getElementById('laperm-info').style.bottom = '0';
+}
+function LaPermClose(){
+    document.getElementById('laperm-head').style.top = '-100%';
+    document.getElementById('laperm-info').style.bottom = '-100%'; 
+}
+
+function BurmillaInfo(){
+    document.getElementById('burmilla-head').style.top = '0';
+    document.getElementById('burmilla-info').style.bottom = '0';
+}
+function BurmillaClose(){
+    document.getElementById('burmilla-head').style.top = '-100%';
+    document.getElementById('burmilla-info').style.bottom = '-100%'; 
+}
+
+function LykoiInfo() {
+    document.getElementById('lykoi-head').style.top = '0';
+    document.getElementById('lykoi-info').style.bottom = '0';
+}
+function LykoiClose() {
+    document.getElementById('lykoi-head').style.top = '-100%';
+    document.getElementById('lykoi-info').style.bottom = '-100%';
+}
+
+function SnowshoeCatInfo() {
+    document.getElementById('snowshoe-head').style.top = '0';
+    document.getElementById('snowshoe-info').style.bottom = '0';
+}
+function SnowshoeCatClose() {
+    document.getElementById('snowshoe-head').style.top = '-100%';
+    document.getElementById('snowshoe-info').style.bottom = '-100%';
+}
+
+function ToygerInfo() {
+    document.getElementById('toyger-head').style.top = '0';
+    document.getElementById('toyger-info').style.bottom = '0';
+}
+function ToygerClose() {
+    document.getElementById('toyger-head').style.top = '-100%';
+    document.getElementById('toyger-info').style.bottom = '-100%';
+}
+
+function ColorpointShorthairInfo() {
+    document.getElementById('colorpoint-head').style.top = '0';
+    document.getElementById('colorpoint-info').style.bottom = '0';
+}
+function ColorpointShorthairClose() {
+    document.getElementById('colorpoint-head').style.top = '-100%';
+    document.getElementById('colorpoint-info').style.bottom = '-100%';
+}
+
+function JavaneseCatInfo() {
+    document.getElementById('javanese-head').style.top = '0';
+    document.getElementById('javanese-info').style.bottom = '0';
+}
+function JavaneseCatClose() {
+    document.getElementById('javanese-head').style.top = '-100%';
+    document.getElementById('javanese-info').style.bottom = '-100%';
+}
+
+function SokokeInfo() {
+    document.getElementById('sokoke-head').style.top = '0';
+    document.getElementById('sokoke-info').style.bottom = '0';
+}
+function SokokeClose() {
+    document.getElementById('sokoke-head').style.top = '-100%';
+    document.getElementById('sokoke-info').style.bottom = '-100%';
+}
+
+function AustralianMistInfo() {
+    document.getElementById('australian-mist-head').style.top = '0';
+    document.getElementById('australian-mist-info').style.bottom = '0';
+}
+function AustralianMistClose() {
+    document.getElementById('australian-mist-head').style.top = '-100%';
+    document.getElementById('australian-mist-info').style.bottom = '-100%';
+}
+
+function KhaoManeeInfo() {
+    document.getElementById('khao-manee-head').style.top = '0';
+    document.getElementById('khao-manee-info').style.bottom = '0';
+}
+function KhaoManeeClose() {
+    document.getElementById('khao-manee-head').style.top = '-100%';
+    document.getElementById('khao-manee-info').style.bottom = '-100%';
+}
+
+function OpenTab(evt, TabName){
+    var TabContent = document.querySelectorAll(".tab-content");
+    TabContent.forEach(function(content){
+        content.classList.remove("active");
+        console.log(TabName);
+    });
+
+    var TabLink = document.querySelectorAll(".tab-link");
+    TabLink.forEach(function(link){
+        link.classList.remove("active");
+    });
+
+    document.getElementById(TabName).classList.add("active");
+    evt.currentTarget.classList.add("active");
+}
+
+document.querySelector(".tab-link").click();
